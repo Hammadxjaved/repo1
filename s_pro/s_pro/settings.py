866b8bf59904e53857dd14123b6ed326ed4c871a
Hammad Javed <111104980+Hammadxjaved@users.noreply.github.com>
2023-05-07 12:02:12 +0530

    Update settings.py

--- s_pro/s_pro/settings.py
+++ s_pro/s_pro/settings.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS = [
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'projectid936@gmail.com'
-EMAIL_HOST_PASSWORD = 'trgfjaiznheutgpv'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 #ds
